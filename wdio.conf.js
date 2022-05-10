5e6a14cace603ecec02bb6f61c55ee64f4f99d2f
urgor <ursa.gorse@ingka.com>
2022-05-11 01:14:35 +0200

    Added headless configuration to support docker running.

--- wdio.conf.js
+++ wdio.conf.js
@@ -57,7 +57,16 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': {
+            args: [
+                '--no-sandbox',
+                '--disable-infobars',
+                '--headless',
+                '--disable-gpu',
+                '--window-size=1440,735'
+            ],
+        }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
@@ -94,7 +103,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://www.volvocars.com/intl/v/car-safety/a-million-more',
+    baseUrl: 'https://www.volvocars.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
